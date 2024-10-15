6983e3e9caf60fe23159208ead1bc847c7504009
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-10-15 20:46:58 -0300

    feat: Create useFactory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,25 @@
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 import { OperatorsModule } from './operators/operators.module';
 import { ProductsModule } from './products/products.module';
 
-const APIKEY = 'DEV-456';
-const APIKEYPROD = 'PROD-12345';
-
 @Module({
-  imports: [OperatorsModule, ProductsModule],
+  imports: [HttpModule, OperatorsModule, ProductsModule, DatabaseModule],
   controllers: [AppController],
   providers: [
     AppService,
     {
-      provide: 'APIKEY',
-      useValue: process.env.NODE_ENV === 'prod' ? APIKEYPROD : APIKEY,
+      provide: 'TAREA_ASINC',
+      useFactory: async (http: HttpService) => {
+        const req = http.get('https://jsonplaceholder.typicode.com/posts');
+        const task = await lastValueFrom(req);
+        return task.data;
+      },
+      inject: [HttpService],
     },
   ],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,8 +2,16 @@ import { Inject, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  constructor(@Inject('APIKEY') private APIKEY: string) {}
+  constructor(
+    @Inject('APIKEY') private APIKEY: string,
+    @Inject('TAREA_ASINC') private TAREA_ASINC: string,
+  ) {}
   getHello(): string {
     return `La llave de la aplicacion es: ${this.APIKEY}`;
+  }
+
+  getUseFactory() {
+    console.log(this.TAREA_ASINC);
+    return 'Realizando una tarea asincrona de ejemplo';
   }
 }
