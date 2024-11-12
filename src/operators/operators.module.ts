6870ead0f2af4f4b4bb0623a24bf473c268bdad0
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-11-12 18:00:12 -0300

    feat: Create operators module

--- src/operators/operators.module.ts
+++ src/operators/operators.module.ts
@@ -1,17 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from 'src/products/entities/product.entity';
-import { ProductsService } from 'src/products/services/products.service';
+
+import { ProductsModule } from 'src/products/products.module';
+
 import { BuyersController } from './controllers/buyers.controller';
 import { OperatorsController } from './controllers/operators.controller';
 import { OrdersController } from './controllers/orders.controller';
+
+import { Product } from 'src/products/entities/product.entity';
+import { Buyer } from './entities/buyer.entity';
 import { Operator } from './entities/operator.entity';
+
+import { ProductsService } from 'src/products/services/products.service';
 import { BuyersService } from './services/buyers.service';
 import { OperatorsService } from './services/operators.service';
 import { OrdersService } from './services/orders.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, Operator])],
+  imports: [
+    ProductsModule,
+    TypeOrmModule.forFeature([Product, Operator, Buyer]),
+  ],
   controllers: [BuyersController, OperatorsController, OrdersController],
   providers: [BuyersService, OperatorsService, OrdersService, ProductsService],
 })
