8ee0c6dad27ea9b6e968976842ea304e8ef2ecf1
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-11-20 20:33:51 -0300

    feat: Update manufacturers module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { CategoriesController } from './controllers/categories.controller';
 import { ManufacturersController } from './controllers/manufacturers.controller';
 import { ProductsController } from './controllers/products.controller';
+
+import { Manufacturer } from './entities/manufacturer.entity';
 import { Product } from './entities/product.entity';
+
 import { CategoriesService } from './services/categories.service';
 import { ManufacturersService } from './services/manufacturers.service';
 import { ProductsService } from './services/products.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product, Manufacturer])],
   controllers: [
     ManufacturersController,
     ProductsController,
